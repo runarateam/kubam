76b857bd9db94690f6986066d214507534fdf994
Badar Wildani <wildanie12@gmail.com>
2025-06-14 22:25:14 +0700

    fix: warning on session null exception

--- functions/session.php
+++ functions/session.php
@@ -1,14 +1,12 @@
 <?php
-
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 $loggedUser = $_SESSION['logged_user'];
 $isLoggedIn = isset($loggedUser);
 
-
-$success = $_SESSION['success'];
-$error = $_SESSION['error'];
-$errors = $_SESSION['errors'];
-$old = $_SESSION['old'];
+$success = $_SESSION['success'] ?? null;
+$error = $_SESSION['error'] ?? null;
+$errors = $_SESSION['errors'] ?? null;
+$old = $_SESSION['old'] ?? null;
 unset($_SESSION['success'], $_SESSION['error'], $_SESSION['errors'], $_SESSION['old']);
